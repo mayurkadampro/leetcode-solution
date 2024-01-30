94cbbd2720358482653d5bf90dd003599609ce83
Mayur Kadam <mayurkadam200@gmail.com>
2024-01-30 19:20:11 +0530

    Time: 65 ms (64.17%) | Memory: 52.4 MB (10.08%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let sum = 0;
+    nums.forEach((item) => (sum += item));
+    let left = 0;
+    for (let i = 0; i < nums.length; i++) {
+        const right = sum - left - nums[i];
+        if (left === right) {
+            return i;
+        }
+        left += nums[i];
+    }
+    return -1;
+};
