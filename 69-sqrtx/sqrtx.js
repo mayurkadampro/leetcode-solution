12b97299f5b4f60ec59bbed4991299a94032fc13
Mayur Kadam <mayurkadam200@gmail.com>
2024-01-31 15:38:04 +0530

    Time: 81 ms (33.20%) | Memory: 52.5 MB (11.13%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -13,20 +13,24 @@ var mySqrt = function (x) {
         return Math.floor(x**(1/2));
     */
 
-    // Third solution
-    let left = 0;
-    let right = x;
-    let result = 0;
-    while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
-        if (mid * mid > x) {
-            right = mid - 1
-        } else if (mid * mid < x) {
-            left = mid + 1;
-            result = mid
-        } else {
-            return mid;
-        }
-    }
-    return result;
+    /* Third solution
+        return Math.floor(Math.sqrt(x));
+    */
+    // Fourth Solution
+    // let left = 0;
+    // let right = x;
+    // let result = 0;
+    // while (left <= right) {
+    //     let mid = left + Math.floor((right - left) / 2);
+    //     if (mid * mid > x) {
+    //         right = mid - 1
+    //     } else if (mid * mid < x) {
+    //         left = mid + 1;
+    //         result = mid
+    //     } else {
+    //         return mid;
+    //     }
+    // }
+    // return result;
+    return Math.floor(Math.sqrt(x));
 };
