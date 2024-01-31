0399b0f916842c0925b70409ff8c2478a81d1e5b
Mayur Kadam <mayurkadam200@gmail.com>
2024-01-31 15:36:30 +0530

    Time: 66 ms (61.28%) | Memory: 53.3 MB (5.33%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -29,16 +29,4 @@ var mySqrt = function (x) {
         }
     }
     return result;
-    // let i = 1, j = x, ans = 0;
-    // while (i <= j) {
-    //     let mid = i + Math.floor((j - i) / 2);
-
-    //     if (mid * mid <= x) {
-    //         i = mid + 1;
-    //         ans = mid;
-    //     }
-    //     else
-    //         j = mid - 1;
-    // }
-    //return ans;
 };
