2952574a5af3d3c80d12df93e5da2562f6ddf326
Mayur Kadam <mayurkadam200@gmail.com>
2024-01-31 12:05:05 +0530

    Time: 230 ms (42.30%) | Memory: 72.7 MB (38.33%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -4,14 +4,14 @@
  */
 var dailyTemperatures = function (temperatures) {
     const idStack = [];
-    const res = new Array(temperatures.length).fill(0);
+    const ans = new Array(temperatures.length).fill(0);
     for (let i = 0; i < temperatures.length; i++) {
         const temp = temperatures[i];
         while (idStack.length && temp > temperatures[idStack[idStack.length - 1]]) {
             const lastId = idStack.pop();
-            res[lastId] = i - lastId;
+            ans[lastId] = i - lastId;
         }
         idStack.push(i);
     }
-    return res;
+    return ans;
 };
