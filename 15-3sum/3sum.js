59d9fb8cf715d69a1dfd7b80cf38ad40804ad9dd
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-01 20:05:05 +0530

    Time: 158 ms (55.16%) | Memory: 65.1 MB (23.05%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,33 +3,28 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-    const results = [];
+    const result = [];
     if (nums.length < 3) return results
-
     nums.sort((a, b) => a - b);
 
-
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] > 0) break;
+    for (let i = 0; i < nums.length; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) continue
-
-        // Two Pointer
-        let left = i + 1;
-        let right = nums.length - 1;
-        while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
-            if (sum > 0) {
-                right--;
-            } else if (sum < 0) {
-                left++;
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0) {
+                j++;
+            } else if (sum > 0) {
+                k--;
             } else {
-                results.push([nums[i], nums[left], nums[right]]);
-                while (nums[left + 1] === nums[left]) left++;
-                while (nums[right - 1] === nums[right]) right--;
-                left++
-                right--
+                result.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                while (j < k && nums[j] === nums[j - 1]) j++;
+                while (j < k && nums[k] === nums[k + 1]) k--;
             }
         }
     }
-    return results
+    return result;
 };
