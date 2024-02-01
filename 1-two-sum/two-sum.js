f04dde2fc2a29d62692f9e3e5be8bc6011f9d5f8
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-01 17:30:01 +0530

    Time: 55 ms (83.90%) | Memory: 52.3 MB (5.05%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,37 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let storeMap = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let diff = target - nums[i]
-        if (storeMap.has(diff)) {
-            return [storeMap.get(diff), i]
+
+    // HashMap Solution
+    // let storeMap = new Map();
+    // for (let i = 0; i < nums.length; i++) {
+    //     let diff = target - nums[i]
+    //     if (storeMap.has(diff)) {
+    //         return [storeMap.get(diff), i]
+    //     }
+    //     storeMap.set(nums[i], i);
+    // }
+    // return [];
+
+    // Two pointer
+    // Create an array of objects {value, index} to keep track of the original indices
+    const numsWithIndices = nums.map((num, index) => ({ value: num, index }));
+    // Sort the array of objects based on the values
+    numsWithIndices.sort((a, b) => a.value - b.value);
+    // Initialize two pointers
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        const currentSum = numsWithIndices[left].value + numsWithIndices[right].value;
+        if (currentSum === target) {
+            // Return the original indices
+            return [numsWithIndices[left].index, numsWithIndices[right].index];
+        } else if (currentSum < target) {
+            left++;
+        } else {
+            right--;
         }
-        storeMap.set(nums[i], i);
     }
+    // No solution found
     return [];
 };
