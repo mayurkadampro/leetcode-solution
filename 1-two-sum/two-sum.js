75005e5bc8759ff344c843f37fbb9d1d8dc7788f
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-01 19:51:16 +0530

    Time: 68 ms (57.21%) | Memory: 52.3 MB (5.05%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,14 +3,38 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let uniqueMap = new Map();
-    for(let i=0; i<nums.length; i++){
-        let diff = target - nums[i];
-        if(uniqueMap.has(diff)){
-            return [uniqueMap.get(diff), i]
+var twoSum = function (nums, target) {
+    // HashMap Solution
+    // let uniqueMap = new Map();
+    // for(let i=0; i<nums.length; i++){
+    //     let diff = target - nums[i];
+    //     if(uniqueMap.has(diff)){
+    //         return [uniqueMap.get(diff), i]
+    //     }
+    //     uniqueMap.set(nums[i], i);
+    // }
+    // return [];
+
+    // Two Pointer
+
+    // Create an array of objects {value, index} to keep track of the original indices
+    const numsWithIndices = nums.map((num, index) => ({ value: num, index }));
+
+    // Sort the array of objects based on the values
+    numsWithIndices.sort((a, b) => a.value - b.value);
+
+    // Initialize two pointers
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        const sum = numsWithIndices[left].value + numsWithIndices[right].value;
+        if (sum < target) {
+            left++;
+        } else if (sum > target) {
+            right--;
+        } else {
+            return [numsWithIndices[left].index, numsWithIndices[right].index]
         }
-        uniqueMap.set(nums[i], i);
     }
     return [];
 };
