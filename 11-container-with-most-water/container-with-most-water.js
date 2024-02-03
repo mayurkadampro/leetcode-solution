57e209a6090547fcd27ae3bbbd9fed85edcc3eb2
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-03 12:45:06 +0530

    Time: 79 ms (19.70%) | Memory: 56.7 MB (21.90%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -1,21 +1,32 @@
 /**
- * @param {number[]} height
+ * @param {number[]} container
  * @return {number}
  */
-var maxArea = function (height) {
-    let result = 0,
-        left = 0,
-        right = height.length - 1;
+var maxArea = function (container) {
+    let left = 0;            // Left pointer starting from the leftmost edge
+    let right = container.length - 1; // Right pointer starting from the rightmost edge
+    let maxWater = 0;        // Initialize the maximum water capacity
 
     while (left < right) {
-        let smallestSide = Math.min(height[left], height[right]);
-        let area = (right - left) * smallestSide;
+        // Calculate the width of the container
+        let width = right - left;
 
-        if (area > result) result = area;
+        // Calculate the height of the container (the minimum height between the two lines)
+        let height = Math.min(container[left], container[right]);
 
-        if (height[left] < height[right]) left++;
-        else right--;
+        // Calculate the water capacity of the current container
+        let water = width * height;
+
+        // Update the maximum water capacity if the current container holds more water
+        maxWater = Math.max(maxWater, water);
+
+        // Move the pointers towards each other
+        if (container[left] < container[right]) {
+            left++;
+        } else {
+            right--;
+        }
     }
 
-    return result;
+    return maxWater;
 };
