6ba364d940015b8202040b50db28b8f519558963
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-05 21:00:58 +0530

    Time: 92 ms (48.67%) | Memory: 53.2 MB (19.46%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -4,10 +4,24 @@
  */
 var firstUniqChar = function (s) {
     if (s.length == 0) return -1;
-    for (let idx = 0; idx < s.length; idx++) {
-        if (s.indexOf(s[idx]) === s.lastIndexOf(s[idx])) {
-            return idx
+    // By inbuild method
+    // for (let idx = 0; idx < s.length; idx++) {
+    //     if (s.indexOf(s[idx]) === s.lastIndexOf(s[idx])) {
+    //         return idx
+    //     }
+    // }
+
+    let mp = {};
+
+    for (let a of s) {
+        mp[a] = (mp[a] || 0) + 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (mp[s[i]] === 1) {
+            return i;
         }
     }
+
     return -1
 };
