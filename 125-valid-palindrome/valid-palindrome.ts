22c3ca933a8624d1869e07239c775ef91621748f
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-12 19:11:26 +0530

    Time: 67 ms (65.12%) | Memory: 52.5 MB (33.37%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -0,0 +1,14 @@
+function isPalindrome(s: string): boolean {
+    if (!s) return false;
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true
+};
