9128bbc931d77a8f69abd81b624032c06ce96adf
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-16 12:23:28 +0530

    Time: 80 ms (21.60%) | Memory: 54.4 MB (19.86%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.ts
+++ 2058-concatenation-of-array/concatenation-of-array.ts
@@ -1,3 +1,10 @@
 function getConcatenation(nums: number[]): number[] {
-    return [...nums, ...nums]
+    // return nums.concat(nums)
+    // return [...nums, ...nums]
+    const result = [];
+    for(let i= 0;  i< nums.length; i++) {
+        result[i] = nums[i];
+        result [i + nums.length] = nums[i]
+    }
+    return result;
 };
