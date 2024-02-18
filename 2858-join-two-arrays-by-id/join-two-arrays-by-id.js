5876c30dda8d0d0e7e3ce46fb98201c682eb56a4
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-18 19:53:53 +0530

    Time: 328 ms (54.16%) | Memory: 92.5 MB (73.34%) - LeetSync

---
+++ 2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+    let map = {};
+    arr1.forEach(obj => map[obj.id] = obj);
+    arr2.forEach((obj) => {
+        if (map[obj.id]) {
+            map[obj.id] = { ...map[obj.id], ...obj };
+        } else {
+            map[obj.id] = obj;
+        }
+    });
+    return [...Object.values(map)];
+};
