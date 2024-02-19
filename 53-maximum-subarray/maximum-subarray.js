51d9d80361cfc40bdb318145e5a83d0db03b304d
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 20:17:06 +0530

    Time: 94 ms (10.28%) | Memory: 57.5 MB (37.15%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -4,22 +4,15 @@
  */
 var maxSubArray = function (nums) {
     if (nums.length === 1) return nums[0];
-
-    // Initialize the max sum...
     let maxSum = nums[0];
-
-    // Traverse all the element through the loop...
-    for (let i = 1; i < nums.length; i++) {
-
-        // nums[i] represents the largest sum of all subarrays ending with index i...
-        // then its value should be the larger one between nums[i]...
-        // nums[i-1] + nums[i] (largest sum plus current number with using prefix)...
-        // calculate nums[0], nums[1]…, nums[n] while comparing each one with current largest sum...
-        nums[i] = Math.max(0, nums[i - 1]) + nums[i];
-
-        // if nums[i] > maxSum then maxSum = nums[i]
-        if (nums[i] > maxSum) {
-            maxSum = nums[i];
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
         }
     }
     return maxSum;
