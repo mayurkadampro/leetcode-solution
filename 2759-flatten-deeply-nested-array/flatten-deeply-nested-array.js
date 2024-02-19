0882dff1741543a583405fbc2f906f92b212b2fa
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 12:42:30 +0530

    Time: 77 ms (96.24%) | Memory: 64.3 MB (80.08%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -4,7 +4,7 @@
  * @return {Array}
  */
 
-//     // 1st Recursive Solution
+// 1st Recursive Solution
 // var flat = function (arr, n) {
 //     let result = [];
 //     var depthFlat = function (arr, count) {
@@ -20,7 +20,7 @@
 //     return depthFlat(arr, 0);
 // };
 
-//     // 2st Recursive Solution
+// 2nd Recursive Solution
 var flat = function (arr, n, tempArray = []) {
     for (let index = 0; index < arr.length; index++) {
         if (Array.isArray(arr[index]) && n > 0) {
@@ -31,4 +31,7 @@ var flat = function (arr, n, tempArray = []) {
     }
     return tempArray
 };
+
+// 3rd Recursive solution by Reduce Method
+
 
