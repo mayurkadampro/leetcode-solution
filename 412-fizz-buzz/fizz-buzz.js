6a96eff2d5a3ef26be03635761be06f2ed202c8f
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 19:29:35 +0530

    Time: 64 ms (33.74%) | Memory: 51.6 MB (44.71%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -3,6 +3,19 @@
  * @return {string[]}
  */
 var fizzBuzz = function (n) {
+
+    // 1st solution
+    // let res = [];
+    // for (let i = 1; i <= n; i++) {
+    //     let string = ''
+    //     if (i % 3 === 0) string += 'Fizz'
+    //     if (i % 5 === 0) string += 'Buzz'
+    //     if (string === '') string += i
+    //     res.push(string)
+    // }
+    // return res;
+
+
     let result = [];
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) result.push("FizzBuzz");
