69d18b92d9203840dabdac0fbb8381871853479f
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 12:43:03 +0530

    Time: 174 ms (20.83%) | Memory: 80.8 MB (39.18%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -21,17 +21,29 @@
 // };
 
 // 2nd Recursive Solution
-var flat = function (arr, n, tempArray = []) {
-    for (let index = 0; index < arr.length; index++) {
-        if (Array.isArray(arr[index]) && n > 0) {
-            flat(arr[index], n - 1, tempArray)
-        } else {
-            tempArray.push(arr[index])
-        }
-    }
-    return tempArray
-};
+// var flat = function (arr, n, tempArray = []) {
+//     for (let index = 0; index < arr.length; index++) {
+//         if (Array.isArray(arr[index]) && n > 0) {
+//             flat(arr[index], n - 1, tempArray)
+//         } else {
+//             tempArray.push(arr[index])
+//         }
+//     }
+//     return tempArray
+// };
 
 // 3rd Recursive solution by Reduce Method
+var flat = (arr, n) =>
+    n ?
+        arr.reduce(
+            (temp, next) => (
+                Array.isArray(next) ?
+                    temp.push(...flat(next, n - 1)) :
+                    temp.push(next),
+                temp
+            ),
+            []
+        ) :
+        arr;
 
 
