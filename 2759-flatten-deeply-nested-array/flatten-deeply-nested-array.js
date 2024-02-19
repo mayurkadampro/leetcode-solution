bc31720f7dace52cee10d79ce9245dc0907924f8
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 12:32:59 +0530

    Time: 87 ms (84.36%) | Memory: 65.7 MB (70.94%) - LeetSync

--- 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
+++ 2759-flatten-deeply-nested-array/flatten-deeply-nested-array.js
@@ -3,19 +3,29 @@
  * @param {number} depth
  * @return {Array}
  */
-var flat = function (arr, n) {
-    // 1st Recursive Solution
-    let result = [];
-    var depthFlat = function (arr, count) {
-        for (let i = 0; i < arr.length; i++) {
-            if (toString.call(arr[i]) === "[object Array]" && count < n) {
-                depthFlat(arr[i], count + 1)
-            } else {
-                result.push(arr[i]);
-            }
+// var flat = function (arr, n) {
+//     // 1st Recursive Solution
+//     let result = [];
+//     var depthFlat = function (arr, count) {
+//         for (let i = 0; i < arr.length; i++) {
+//             if (toString.call(arr[i]) === "[object Array]" && count < n) {
+//                 depthFlat(arr[i], count + 1)
+//             } else {
+//                 result.push(arr[i]);
+//             }
+//         }
+//         return result;
+//     }
+//     return depthFlat(arr, 0);
+// };
+var flat = function (arr, n, tempArray = [], currentCycle = 0) {
+    for (let index = 0; index < arr.length; index++) {
+        if (Array.isArray(arr[index]) && currentCycle < n) {
+            flat(arr[index], n, tempArray, currentCycle + 1)
+        } else {
+            tempArray.push(arr[index])
         }
-        return result;
     }
-    return depthFlat(arr, 0);
+    return tempArray
 };
 
