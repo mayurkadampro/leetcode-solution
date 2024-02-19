5fb1dbf26d941db6bb92021e8220a1e807fd904b
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-19 19:39:50 +0530

    Time: 57 ms (46.17%) | Memory: 51.2 MB (12.89%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -5,16 +5,13 @@
 var diagonalSum = function (mat) {
     let matLength = mat[0].length;
 
-    // Calulate Primary Diagonal
+    // Calulate Primary & Secondary Diagonal Sum
     let sum = 0
     for (let i = 0; i < matLength; i++) {
         sum += mat[i][i];
-    }
-
-    // Calulate Secondary Diagonal
-    for (let i = 0; i < matLength; i++) {
         sum += mat[i][mat[i].length - i - 1];
     }
+
     if (matLength % 2 !== 0) return sum - mat[Math.floor(matLength / 2)][Math.floor(matLength / 2)]
     return sum;
 };
