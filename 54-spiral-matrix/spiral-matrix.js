f19120871a8e0b74b51a84718297b89bb8ade39f
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-21 19:52:14 +0530

    Time: 54 ms (42.65%) | Memory: 49.2 MB (5.77%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.js
+++ 54-spiral-matrix/spiral-matrix.js
@@ -4,34 +4,35 @@
  */
 var spiralOrder = function (matrix) {
     const res = [];
-    let d = 0;
-
     let left = 0;
     let right = matrix[0].length - 1;
     let top = 0;
     let bottom = matrix.length - 1;
 
     while (left <= right && top <= bottom) {
-        if (d == 0) {
-            for (let i = left; i <= right; i++) {
-                res.push(matrix[top][i]);
-            }
-            d = 1; top++;
-        } else if (d == 1) {
-            for (let i = top; i <= bottom; i++) {
-                res.push(matrix[i][right]);
-            }
-            d = 2; right--;
-        } else if (d == 2) {
+
+        for (let i = left; i <= right; i++) {
+            res.push(matrix[top][i]);
+        }
+        top++;
+
+
+        for (let i = top; i <= bottom; i++) {
+            res.push(matrix[i][right]);
+        }
+        right--;
+
+        if (left <= right && top <= bottom) {
             for (let i = right; i >= left; i--) {
                 res.push(matrix[bottom][i]);
             }
-            d = 3; bottom--;
-        } else if (d == 3) {
+            bottom--;
+        }
+        if (left <= right && top <= bottom) {
             for (let i = bottom; i >= top; i--) {
                 res.push(matrix[i][left]);
             }
-            d = 0; left++;
+            left++;
         }
     }
     return res;
