4280b50f14ecebbcef55fcb448057025aa941135
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-28 19:38:24 +0530

    Time: 50 ms (73.03%) | Memory: 48.7 MB (36.64%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.js
+++ 1635-number-of-good-pairs/number-of-good-pairs.js
@@ -3,13 +3,27 @@
  * @return {number}
  */
 var numIdenticalPairs = function (nums) {
-    let pairNum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++){
-            if(i < j && nums[i] === nums[j]){
-                pairNum += 1;
-            }
+    // 1st solution time O(N^2) space O(1)
+    // let pairNum = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     for (let j = 0; j < nums.length; j++){
+    //         if(i < j && nums[i] === nums[j]){
+    //             pairNum += 1;
+    //         }
+    //     }
+    // }
+    // return pairNum;
+
+    // 2nd solution time O(N) space O(N)
+    const map = {}
+    let count = 0;
+    for (const number of nums) {
+        if (map[number]) {
+            count += map[number];
+            map[number] += 1;
+        } else {
+            map[number] = 1;
         }
     }
-    return pairNum;
+    return count
 };
