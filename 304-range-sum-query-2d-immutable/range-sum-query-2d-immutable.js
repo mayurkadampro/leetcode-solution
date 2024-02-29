7335e59f530fb18e87e808d8a43d9c1b0a22838d
Mayur Kadam <mayurkadam200@gmail.com>
2024-02-29 18:36:12 +0530

    Time: 403 ms (23.17%) | Memory: 79 MB (33.74%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function (matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    const sum = [...Array(m)].map(_ => [...Array(n)].fill(0));
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            sum[i][j] = (sum[i - 1]?.[j] || 0) + (sum[i]?.[j - 1] || 0) + matrix[i][j] - (sum[i - 1]?.[j - 1] || 0);
+        }
+    }
+    this.sum = sum;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function (row1, col1, row2, col2) {
+    return this.sum[row2][col2] - (this.sum[row1 - 1]?.[col2] || 0) - (this.sum[row2]?.[col1 - 1] || 0) +
+        (this.sum[row1 - 1]?.[col1 - 1] || 0);
+};
+
+/** 
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
