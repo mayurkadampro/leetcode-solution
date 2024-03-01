bcea93197f1486fd38ab645373b32f7b0a8f7991
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-01 12:10:03 +0530

    Time: 57 ms (18.31%) | Memory: 48.9 MB (12.68%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
@@ -6,12 +6,8 @@ var maxChunksToSorted = function (arr) {
     let max = 0;
     let chunkCount = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (max < arr[i]) {
-            max = arr[i];
-        }
-        if (max === i) {
-            chunkCount++;
-        };
+        max = Math.max(arr[i], max);
+        if (max === i) chunkCount++;
     }
     return chunkCount;
 };
