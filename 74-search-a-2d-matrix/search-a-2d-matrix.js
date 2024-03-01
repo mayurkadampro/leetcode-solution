5ae5b1e1de710bc9087174021d85a711eed983a9
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-01 19:06:50 +0530

    Time: 44 ms (92.50%) | Memory: 48.8 MB (55.01%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -6,23 +6,23 @@
 var searchMatrix = function (matrix, target) {
 
     // 1st solution - Diagonal search from top right - O(m + n)
-    // if (matrix.length < 1) return false;
+    if (matrix.length < 1) return false;
 
-    // //start with top-right element
-    // var row = 0;
-    // var col = matrix[0].length - 1;
+    //start with top-right element
+    var row = 0;
+    var col = matrix[0].length - 1;
 
-    // //loop till row and column number are within bounds
-    // while (row <= matrix.length - 1 && col >= 0) {
-    //     if (matrix[row][col] > target) {
-    //         col--;
-    //     } else if (matrix[row][col] < target) {
-    //         row++;
-    //     } else {
-    //         return true;
-    //     }
-    // }
-    // return false;
+    //loop till row and column number are within bounds
+    while (row <= matrix.length - 1 && col >= 0) {
+        if (matrix[row][col] > target) {
+            col--;
+        } else if (matrix[row][col] < target) {
+            row++;
+        } else {
+            return true;
+        }
+    }
+    return false;
 
     // 2nd solution - Binary search in each row - O(mlog n)
     // for (let i = 0; i < matrix.length; i++) {
@@ -43,18 +43,18 @@ var searchMatrix = function (matrix, target) {
     // return false;
 
     // 3rd solution - Binary search complete matrix - O(log mn)
-    let left = 0;
-    let right = (matrix.length * matrix[0].length) - 1;
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let midNum = matrix[Math.floor(mid / matrix[0].length)][mid % matrix[0].length];
-        if (midNum > target) {
-            right = mid - 1;
-        } else if (midNum < target) {
-            left = mid + 1;
-        } else {
-            return true;
-        }
-    }
-    return false;
+    // let left = 0;
+    // let right = (matrix.length * matrix[0].length) - 1;
+    // while (left <= right) {
+    //     let mid = Math.floor((left + right) / 2);
+    //     let midNum = matrix[Math.floor(mid / matrix[0].length)][mid % matrix[0].length];
+    //     if (midNum > target) {
+    //         right = mid - 1;
+    //     } else if (midNum < target) {
+    //         left = mid + 1;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+    // return false;
 };
