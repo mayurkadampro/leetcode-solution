1276747f56d057e6bffeab4ca7773b7efd6c971b
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-03 13:12:57 +0530

    Time: 155 ms (78.91%) | Memory: 66.7 MB (43.73%) - LeetSync

--- 15-3sum/3sum.ts
+++ 15-3sum/3sum.ts
@@ -4,11 +4,9 @@ function threeSum(nums: number[]): number[][] {
     nums.sort((a, b) => a - b);
 
     for (let i = 0; i < nums.length; i++) {
-
         if (i > 0 && nums[i] === nums[i - 1]) {
             continue
         }
-
         let j = i + 1;
         let k = nums.length - 1;
         while (j < k) {
