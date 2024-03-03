4105b1f3ea0be81b470c03a03553bcaef973ed9f
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-03 14:26:46 +0530

    Time: 61 ms (71.71%) | Memory: 52.6 MB (20.32%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.js
+++ 898-transpose-matrix/transpose-matrix.js
@@ -3,7 +3,9 @@
  * @return {number[][]}
  */
 var transpose = function (matrix) {
-    const result = Array.from({ length: matrix[0].length }, () => Array(matrix.length).fill(0));
+    const row = matrix.length;
+    const col = matrix[0].length;
+    const result = new Array(col).fill(0).map(() => new Array(row).fill(0));
     for (let r = 0; r < matrix.length; r++) {
         for (let c = 0; c < matrix[0].length; c++) {
             result[c][r] = matrix[r][c];
