92ab66ec007db05471844b17e84363d4d2393d7a
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-04 20:12:08 +0530

    Time: 51 ms (50.39%) | Memory: 49 MB (9.33%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.js
+++ 168-excel-sheet-column-title/excel-sheet-column-title.js
@@ -3,17 +3,29 @@
  * @return {string}
  */
 var convertToTitle = function (columnNumber) {
-    if (columnNumber < 27) return String.fromCharCode(columnNumber + 64);
-    let result = '';
+    // if (columnNumber < 27) return String.fromCharCode(columnNumber + 64);
+    // let result = '';
+    // while (columnNumber > 0) {
+    //     let r = columnNumber % 26;
+    //     let q = parseInt(columnNumber / 26);
+    //     if (r === 0) {
+    //         r = 26;
+    //         q--;
+    //     }
+    //     result = String.fromCharCode(64 + r) + result;
+    //     columnNumber = q;
+    // }
+    // return result;
+    let str = '';
+
     while (columnNumber > 0) {
-        let r = columnNumber % 26;
-        let q = parseInt(columnNumber / 26);
-        if (r === 0) {
-            r = 26;
-            q--;
-        }
-        result = String.fromCharCode(64 + r) + result;
-        columnNumber = q;
+
+        columnNumber--;
+        str += String.fromCharCode((columnNumber % 26) + 65);
+        columnNumber = Math.floor(columnNumber / 26);
     }
-    return result;
+
+    // Reverse the string.
+    str = str.split('').reverse().join('');
+    return str;
 };
