d961a8395c95082e77bc347b88f557e4f8734754
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-05 11:53:06 +0530

    Time: 50 ms (59.74%) | Memory: 49.1 MB (5.19%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.js
+++ 866-rectangle-overlap/rectangle-overlap.js
@@ -6,5 +6,15 @@
 var isRectangleOverlap = function (rec1, rec2) {
     const [aX1, aY1, aX2, aY2] = rec1;
     const [bX1, bY1, bX2, bY2] = rec2;
-    return aX1 < bX2 && aX2 > bX1 && aY1 < bY2 && aY2 > bY1;
+
+    let left = Math.max(aX1, bX1);
+    let bottom = Math.max(aY1, bY1);
+
+    let right = Math.min(aX2, bX2);
+    let top = Math.min(aY2, bY2);
+
+    if (right > left && top > bottom) {
+        return true;
+    }
+    return false;
 };
