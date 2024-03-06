09f3fc8f56c027b68fd6e843293d42dead53cf08
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-06 11:13:58 +0530

    Time: 91 ms (44.71%) | Memory: 58.9 MB (70.59%) - LeetSync

---
+++ 903-implement-rand10-using-rand7/implement-rand10-using-rand7.js
@@ -0,0 +1,15 @@
+/**
+ * The rand7() API is already defined for you.
+ * var rand7 = function() {}
+ * @return {number} a random integer in the range 1 to 7
+ */
+var rand10 = function () {
+    while (true) {
+        const num1 = rand7() - 1;
+        const num2 = rand7();
+        const combinedNum = num1 * 7 + num2;
+        if (combinedNum <= 40) {
+            return (combinedNum % 10) + 1;
+        }
+    }
+};
