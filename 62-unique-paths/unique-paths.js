b3d42bde08bb7f4d350244ea6551992611a07147
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-07 17:41:44 +0530

    Time: 46 ms (91.65%) | Memory: 49.6 MB (26.59%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -6,11 +6,13 @@
 var uniquePaths = function (m, n) {
     let pathArray = new Array(m).fill(1).map(() => new Array(n).fill(1));
     let row = pathArray.length;
-    let col = pathArray[0].length
+    let col = pathArray[0].length;
+
     for (let i = 1; i < row; i++) {
         for (let j = 1; j < col; j++) {
             pathArray[i][j] = pathArray[i - 1][j] + pathArray[i][j - 1];
         }
     }
+
     return pathArray[row - 1][col - 1];
 };
