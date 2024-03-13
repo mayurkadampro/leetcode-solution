2f33bbfb422f482a3c7340a8fc6696e0aee46e5b
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-13 11:10:43 +0530

    Time: 68 ms (89.83%) | Memory: 61.5 MB (81.36%) - LeetSync

---
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+var findPeakGrid = function (mat) {
+    let left = 0;
+    let right = mat[0].length - 1;
+    let localPeakRow = 0;
+
+    while (left <= right) {
+        //mid col
+        let midCol = left + Math.floor((right - left) / 2);
+
+        //find max value of mid col and update its row
+        for (let row = 0; row < mat.length; row++) {
+            localPeakRow = mat[localPeakRow][midCol] >= mat[row][midCol] ? localPeakRow : row
+        }
+
+
+        if (mat[localPeakRow][midCol + 1] && mat[localPeakRow][midCol + 1] > mat[localPeakRow][midCol]) { //the local peak on the right
+            left = midCol + 1
+        } else if (mat[localPeakRow][midCol - 1] && mat[localPeakRow][midCol - 1] > mat[localPeakRow][midCol]) { //the local peak on the left
+            right = midCol - 1
+        } else { //gotcha
+            return [localPeakRow, midCol]
+        }
+    }
+    return [1, -1]; // Return this outside the while loop
+};
+
+function findMax(mat) {
+    let max = 0;
+    for (let i = 1; i < mat.length; i++) {
+        if (mat[i] > mat[max]) {
+            max = i;
+        }
+    }
+    return max;
+}
