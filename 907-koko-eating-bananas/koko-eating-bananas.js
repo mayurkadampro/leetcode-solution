fbe6268301619b690e5948509b7586280570d4cf
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-15 22:04:10 +0530

    Time: 71 ms (42.89%) | Memory: 52.7 MB (34.22%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+    let left = 0;
+    let right = findMax(piles);
+
+
+    while (left < right) {
+        let k = Math.floor(left + (right - left) / 2);
+        let eatBananaCount = eatBanana(piles, k);
+        if (eatBananaCount > h) {
+            left = k + 1;
+        } else {
+            right = k;
+        }
+    }
+    return right;
+};
+
+var eatBanana = (piles, k) => {
+    let maxEat = 0;
+    for (let pile of piles) {
+        maxEat += Math.ceil(pile / k);
+    }
+    return maxEat;
+}
+
+var findMax = (piles) => {
+    let max = piles[0];
+    for (let i = 1; i < piles.length; i++) {
+        if (piles[i] > max) {
+            max = piles[i]
+        }
+    }
+    return max;
+}
