6e72f3598a8ed7f05f54a35fdb4e8633066f4270
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-22 11:51:35 +0530

    Time: 53 ms (58.22%) | Memory: 48.5 MB (90.59%) - LeetSync

---
+++ 50-powx-n/powx-n.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function (x, n) {
+    // Base Case
+    if (n === 0) return 1;
+    if (x === 0) return 0;
+
+    const half = myPow(x, Math.floor(Math.abs(n) / 2));
+    let result = n % 2 == 0 ? half * half : half * half * x;
+    return result = n >= 0 ? result : 1 / result
+};
