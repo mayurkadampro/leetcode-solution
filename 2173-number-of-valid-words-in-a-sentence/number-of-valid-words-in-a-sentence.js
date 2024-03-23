dc0eba39608623a7a1a55b251d1021a4fa7de27f
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-23 18:17:10 +0530

    Time: 65 ms (35.25%) | Memory: 51.5 MB (31.97%) - LeetSync

--- 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.js
+++ 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.js
@@ -4,12 +4,12 @@
  */
 var countValidWords = function (sentence) {
     let list = sentence.split(' ');
-    let filtered = list.filter(s => {
-        if (/\d/.test(s) || s === '') return false   //removes anything with numbers or is blank
-        if (/^[!,.]$/.test(s)) return true          //punctuation only
-        if (/^\w+[!,.]?$/.test(s)) return true      //word + optional punctuation
-        if (/^\w+[-]?\w+[!,.]?$/.test(s)) return true //word + optional hypen + word + optional punctuation
+    let filtered = list.filter((word) => {
+        if (/\d/.test(word) || word === '') return false; // removes anything with numbers or is blank
+        if (/^[!,.]$/.test(word)) return true; // punctuation only
+        if (/^\w+[!,.]?$/.test(word)) return true; // word + optional punctuation
+        if (/^\w+[-]?\w+[!,.]?$/.test(word)) return true //word + optional hypen + word + optional punctuation
         return false
-    });
+    })
     return filtered.length;
 };
