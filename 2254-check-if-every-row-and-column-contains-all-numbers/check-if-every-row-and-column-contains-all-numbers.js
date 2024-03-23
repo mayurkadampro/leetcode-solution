9a35b6548fae59800334096185541e4fa7813782
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-23 15:47:23 +0530

    Time: 99 ms (43.58%) | Memory: 56.5 MB (78.21%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.js
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.js
@@ -3,12 +3,17 @@
  * @return {boolean}
  */
 var checkValid = function (matrix) {
-    for (let i = 0; i <= matrix.length - 1; i++) {
+    let row = matrix.length - 1;
+    let col = matrix[0].length - 1;
+
+    for (let i = 0; i <= row; i++) {
         const rowSet = new Set();
         const colSet = new Set();
-        for (let j = 0; j <= matrix.length - 1; j++) {
+
+        for (let j = 0; j <= col; j++) {
             if (rowSet.has(matrix[i][j])) return false;
             if (colSet.has(matrix[j][i])) return false;
+            
             rowSet.add(matrix[i][j]);
             colSet.add(matrix[j][i]);
         }
