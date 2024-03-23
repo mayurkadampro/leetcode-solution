4dc10fc2ab9ec7bbb4c5c513fcf72678d7eb2200
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-23 18:19:12 +0530

    Time: 53 ms (83.61%) | Memory: 50.2 MB (99.18%) - LeetSync

--- 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.js
+++ 2173-number-of-valid-words-in-a-sentence/number-of-valid-words-in-a-sentence.js
@@ -3,13 +3,23 @@
  * @return {number}
  */
 var countValidWords = function (sentence) {
-    let list = sentence.split(' ');
-    let filtered = list.filter((word) => {
-        if (/\d/.test(word) || word === '') return false; // removes anything with numbers or is blank
-        if (/^[!,.]$/.test(word)) return true; // punctuation only
-        if (/^\w+[!,.]?$/.test(word)) return true; // word + optional punctuation
-        if (/^\w+[-]?\w+[!,.]?$/.test(word)) return true //word + optional hypen + word + optional punctuation
-        return false
-    })
-    return filtered.length;
+    // 1 st solution
+    // let list = sentence.split(' ');
+    // let filtered = list.filter((word) => {
+    //     if (/\d/.test(word) || word === '') return false; // removes anything with numbers or is blank
+    //     if (/^[!,.]$/.test(word)) return true; // punctuation only
+    //     if (/^\w+[!,.]?$/.test(word)) return true; // word + optional punctuation
+    //     if (/^\w+[-]?\w+[!,.]?$/.test(word)) return true //word + optional hypen + word + optional punctuation
+    //     return false
+    // })
+    // return filtered.length;
+
+    let validPattern = /^([a-z]+(-?[a-z]+)?)?(!|\.|,)?$/;
+    sentence = sentence.trim().split(/\s+/);
+    return sentence.reduce((acc, word) => {
+        if (validPattern.test(word)) {
+            return acc + 1;
+        }
+        return acc;
+    }, 0);
 };
