84d1eec694cf894e49967d6f540ff5ab54cbd5bc
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-23 14:28:02 +0530

    Time: 60 ms (25.25%) | Memory: 50.5 MB (83.66%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.js
@@ -3,23 +3,12 @@
  * @return {number}
  */
 var diagonalSum = function (mat) {
-    // let matLength = mat[0].length;
-    // // Calulate Primary & Secondary Diagonal Sum
-    // let sum = 0;
-    // for (let i = 0; i < matLength; i++) {
-    //     sum += mat[i][i];
-    //     sum += mat[i][mat[i].length - i - 1];
-    // }
-    // if (matLength % 2 !== 0) return sum - mat[Math.floor(matLength / 2)][Math.floor(matLength / 2)];
-    // return sum;
-
+    let row = mat.length - 1;
+    let col = mat[0].length - 1;
     let sum = 0;
-    let j = mat[0].length - 1;
-    for (let i = 0; i < mat.length; i++, j--) {
-        if (i !== j) {
-            sum += mat[i][j];
-        }
-        sum += mat[i][i]
+    for (let i = 0; i <= row; i++, col--) {
+        sum += mat[i][i];
+        sum += mat[i][col];
     }
-    return sum;
+    return row % 2 === 0 ? sum - mat[Math.floor(row / 2)][Math.floor(row / 2)] : sum;
 };
