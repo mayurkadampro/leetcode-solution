8225b86fd9c4839d386dd05fe7413f99c70b269c
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-23 16:16:59 +0530

    Time: 60 ms (86.68%) | Memory: 51.5 MB (86.22%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -11,20 +11,26 @@ var isValidSudoku = function (board) {
         for (let j = 0; j < board.length; j++) {
             let row = board[i][j];
             let col = board[j][i];
-
-            if (row !== '.' && rowsSet.has(row)) return false;
-            rowsSet.add(row);
-
-            if (col !== '.' && colsSet.has(col)) return false;
-            colsSet.add(col);
-
             let box = board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
 
-            if (box !== '.' && boxsSet.has(box)) return false;
-            boxsSet.add(box);
+            if (row !== '.' && rowsSet.has(row)) {
+                return false;
+            } else {
+                rowsSet.add(row);
+            }
+
+            if (col !== '.' && colsSet.has(col)) {
+                return false;
+            } else {
+                colsSet.add(col);
+            }
+
+            if (box !== '.' && boxsSet.has(box)) {
+                return false;
+            } else {
+                boxsSet.add(box);
+            }
         }
-
-
     }
     return true;
 };
