9f784f4a026d75f3860181606edf3332f0feb260
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-24 12:28:13 +0530

    Time: 108 ms (27.89%) | Memory: 76.4 MB (6.68%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/find-the-duplicate-number.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
+var findDuplicate = function (nums) {
     let keyMap = new Map();
-
-    for(let element of nums){
-        if(keyMap.has(element)){
+    for (let element of nums) {
+        if (keyMap.has(element)) {
             return element;
         }
         keyMap.set(element)
     }
+    return -1;  // Just to satisfy the compiler, this should never be reached
 };
