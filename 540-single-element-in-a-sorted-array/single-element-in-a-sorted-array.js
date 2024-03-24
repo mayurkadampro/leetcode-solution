c92b125658c1c67cd6704c6b37282b3ee23543cc
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-24 18:53:21 +0530

    Time: 58 ms (38.08%) | Memory: 51.6 MB (70.60%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -8,15 +8,11 @@ var singleNonDuplicate = function (nums) {
 
     while (left <= right) {
         let mid = Math.floor(left + (right - left) / 2);
-        if (nums[mid] !== nums[mid - 1] && nums[mid] !== nums[mid + 1]) {
-            return nums[mid];
-        }
-        if ((mid % 2 === 0 && nums[mid] === nums[mid + 1]) ||
-            (mid % 2 !== 0 && nums[mid] !== nums[mid + 1])) {
+        if (nums[mid] === nums[mid ^ 1]) {
             left = mid + 1;
         } else {
-            right = mid - 1;
+            right = mid - 1
         }
     }
-    return -1;
+    return nums[left];
 };
