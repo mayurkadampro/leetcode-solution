dcf1da23f0f5727ebc6b223c1b96eb54e6778ebf
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-26 17:05:02 +0530

    Time: 47 ms (86.66%) | Memory: 49.2 MB (32.31%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,11 +6,35 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    let count = 0;
-    for (let i = m; count < n; i++, count++) {
-        nums1[i] = nums2[count];
+    // O((m+n)log(m+n))
+    // for (let i = m, count = 0; count < n; i++, count++) {
+    //     nums1[i] = nums2[count];
+    // }
+    // nums1 = nums1.sort((a, b) => a - b);
+    // return nums1;
+
+    // Two Pointer - O(m+n)
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] < nums2[j]) {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        } else {
+            nums1[k] = nums1[i];
+            k--;
+            i--;
+        }
     }
 
-    nums1 = nums1.sort((a, b) => a - b);
-    return nums1;
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        k--;
+        j--;
+    }
+
+    
+
 };
