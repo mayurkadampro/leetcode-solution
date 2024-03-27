8701543a40d6dcc52e87ccdd46c3dd157069437a
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-27 12:10:53 +0530

    Time: 60 ms (90.57%) | Memory: 54.3 MB (81.67%) - LeetSync

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function (nums, k) {
+    if (k <= 1) return 0;
+    if (nums === null || nums.length === 0) {
+        return 0;
+    }
+    let start = 0, product = 1, count = 0;
+
+    for (let end = 0; end < nums.length; end++) {
+        product *= nums[end];
+        // ensure subarray product is less than k
+        while (product >= k && start <= end) {
+            product /= nums[start++];
+        }
+        count += end - start + 1;
+    }
+    return count;
+};
