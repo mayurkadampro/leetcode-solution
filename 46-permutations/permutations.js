f64afb67b68e5c422df320b9337d9af8d361e2b2
Mayur Kadam <mayurkadam200@gmail.com>
2024-03-30 16:06:46 +0530

    Time: 66 ms (73.92%) | Memory: 53.5 MB (48.49%) - LeetSync

--- 46-permutations/permutations.js
+++ 46-permutations/permutations.js
@@ -32,3 +32,30 @@ function permutations(current, remaining, result) {
 
 
 // Alternate Solution
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var permute = function (nums) {
+//     let result = [];
+//     permutations([], nums, result);
+//     return result;
+// };
+
+// function permutations(current, remaining, result) {
+//     if (remaining.length <= 0) result.push(current.slice());
+//     else {
+//         // Loop through remaining elements
+//         for (let i = 0; i < remaining.length; i++) {
+//             // Insert the iTH element onto the end of current
+//             current.push(remaining[i]);
+
+//             // Recurse with inserted element removed
+//             permutations(current.slice(), remaining.slice(0, i).concat(remaining.slice(i + 1)), result);
+
+//             // Remove last inserted element for next iteration
+//             current.pop();
+
+//         }
+//     }
+// }
