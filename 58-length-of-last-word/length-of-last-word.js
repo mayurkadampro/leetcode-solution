7c302ae8044b76638c7f415638e0350d2dc7aa04
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-01 11:12:00 +0530

    Time: 65 ms (5.67%) | Memory: 48.9 MB (37.17%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -2,7 +2,11 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    let arr = s.trim().split(" ");
-    return arr[arr.length-1].length;
+var lengthOfLastWord = function (s) {
+    // 1st solution
+    // let arr = s.trim().split(" ");
+    // return arr[arr.length-1].length;
+
+    // 2nd solution
+    return s.trim().split(" ").pop().length;
 };
