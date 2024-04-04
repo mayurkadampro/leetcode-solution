0727cc4337ed435bca0a5bc00399e27668680600
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-04 19:08:30 +0530

    Time: 43 ms (97.89%) | Memory: 51.3 MB (44.37%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -5,14 +5,14 @@
 var countNegatives = function (grid) {
     let row = grid.length - 1;
     let col = 0;
-    let res = 0;
+    let count = 0;
     while (row >= 0 && col < grid[0].length) {
         if (grid[row][col] >= 0) {
             col++;
         } else {
-            res += grid[0].length - col;
+            count += grid[0].length - col;
             row--;
         }
     }
-    return res;
+    return count;
 };
