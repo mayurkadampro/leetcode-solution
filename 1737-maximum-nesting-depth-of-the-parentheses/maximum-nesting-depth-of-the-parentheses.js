4ad589ff33753ee211ee7b278f3948f486f7b496
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-04 23:25:53 +0530

    Time: 56 ms (40.58%) | Memory: 49.1 MB (41.16%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -2,16 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var maxDepth = function(s) {
+var maxDepth = function (s) {
     let result = 0;
     let count = 0;
-    for(let c of s){
-        if(c === "("){
-            count += 1;
-        }else if(c === ")"){
-            count -= 1;
+    for (let c of s) {
+        if (c === "(") {
+            count++;
+            if (result < count) {
+                result = count;
+            }
+        } else if (c === ")") {
+            count--;
         }
-        result = Math.max(count, result);
     }
     return result;
 };
