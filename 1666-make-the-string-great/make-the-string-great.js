0750e093443aecd6b775d61ea166d94828ef5ecf
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-05 11:34:53 +0530

    Time: 53 ms (84.23%) | Memory: 50.8 MB (68.92%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -3,13 +3,13 @@
  * @return {string}
  */
 var makeGood = function (s) {
-    let result = [];
+    let result = "";
     for (let c of s) {
-        if (result.length && Math.abs(c.charCodeAt() - result[result.length - 1].charCodeAt()) === 32) {
-            result.pop();
+        if (result.length && Math.abs(c.charCodeAt() - result.slice(-1).charCodeAt()) === 32) {
+            result = result.slice(0, result.length - 1);
         } else {
-            result.push(c);
+            result += c;
         }
     }
-    return result.join('');
+    return result;
 };
