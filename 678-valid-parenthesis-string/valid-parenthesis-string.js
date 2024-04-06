a5e7bb7f51b1a6d896e2aa81d09532e78b85446b
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-07 00:31:10 +0530

    Time: 45 ms (88.56%) | Memory: 48.8 MB (66.42%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -3,38 +3,60 @@
  * @return {boolean}
  */
 var checkValidString = function (s) {
-    let stack = [];
-    for (let i = 0; i < s.length; i++) {
-        const char = s[i];
-        if (char === "(" || char === "*") {
-            stack.push("(");
-        } else if (char === ")") {
-            if (stack.length < 1) {
-                return false;
-            } else {
-                stack.pop();
-            }
-        }
-    };
+    // let stack = [];
+    // for (let i = 0; i < s.length; i++) {
+    //     const char = s[i];
+    //     if (char === "(" || char === "*") {
+    //         stack.push("(");
+    //     } else if (char === ")") {
+    //         if (stack.length < 1) {
+    //             return false;
+    //         } else {
+    //             stack.pop();
+    //         }
+    //     }
+    // };
 
-    stack = [];
-    for (let i = s.length - 1; i >= 0; i--) {
-        const char = s[i];
-        if (char === ")" || char === "*") {
-            stack.push(")");
-        } else if (char === "(") {
-            if (stack.length < 1) {
-                return false;
-            } else {
-                stack.pop();
-            }
-        }
-    };
+    // stack = [];
+    // for (let i = s.length - 1; i >= 0; i--) {
+    //     const char = s[i];
+    //     if (char === ")" || char === "*") {
+    //         stack.push(")");
+    //     } else if (char === "(") {
+    //         if (stack.length < 1) {
+    //             return false;
+    //         } else {
+    //             stack.pop();
+    //         }
+    //     }
+    // };
 
-    return true;
+    // return true;
 
     // Gready solution
     let left_min = 0;
     let left_max = 0;
-    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(') {
+            left_min++;
+            left_max++;
+        }
+        if (s[i] == ')') {
+            left_min--;
+            left_max--;
+
+        }
+        if (s[i] == '*') {
+            left_min--;
+            left_max++;
+
+        }
+        if (left_max < 0) {
+            return false;
+        }
+        if (left_min < 0) {
+            left_min = 0;
+        }
+    }
+    return left_min === 0;
 };
