bb31f89792d203d006a2d6597133f8cc3ce4f90d
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-08 16:11:30 +0530

    Time: 125 ms (92.23%) | Memory: 63.3 MB (66.43%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.js
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.js
@@ -3,18 +3,36 @@
  * @return {number[][]}
  */
 var minimumAbsDifference = function (arr) {
-    const ans = [];
-    arr.sort((a, b) => a - b);
+    // 1st solution
+    // const ans = [];
+    // arr.sort((a, b) => a - b);
+    // let minDiff = Infinity;
+
+    // for (let i = 1; i < arr.length; i++) {
+    //     minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+    // }
+
+    // for (let i = 1; i < arr.length; i++) {
+    //     if (arr[i] - arr[i - 1] === minDiff) {
+    //         ans.push([arr[i - 1], arr[i]]);
+    //     }
+    // }
+    // return ans;
+
+
     let minDiff = Infinity;
+    let output = [];
+    arr.sort((a, b) => a - b);
 
     for (let i = 1; i < arr.length; i++) {
-        minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
-    }
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] - arr[i - 1] === minDiff) {
-            ans.push([arr[i - 1], arr[i]]);
+        const diff = arr[i] - arr[i - 1];
+        if (diff < minDiff) {
+            minDiff = diff;
+            output = [[arr[i - 1], arr[i]]];
+        } else if (diff === minDiff) {
+            output.push([arr[i - 1], arr[i]]);
         }
     }
-    return ans;
+
+    return output;
 };
