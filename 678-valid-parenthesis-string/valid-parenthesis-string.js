557dca4ce5ec492b928955fe605b26808068f1f3
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-08 11:33:20 +0530

    Time: 50 ms (68.16%) | Memory: 48.8 MB (64.42%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -3,35 +3,65 @@
  * @return {boolean}
  */
 var checkValidString = function (s) {
-    let stack = [];
+    // Simple Login
+    let openStack = [];
+    let asteriskStack = [];
+
     for (let i = 0; i < s.length; i++) {
         const char = s[i];
-        if (char === "(" || char === "*") {
-            stack.push("(");
-        } else if (char === ")") {
-            if (stack.length < 1) {
-                return false;
+        if (char === "(") {
+            openStack.push(i);
+        } else if (char === "*") {
+            asteriskStack.push(i);
+        } else {
+            if (openStack.length) {
+                openStack.pop();
+            } else if (asteriskStack.length) {
+                asteriskStack.pop();
             } else {
-                stack.pop();
+                return false;
             }
         }
-    };
+    }
 
-    stack = [];
-    for (let i = s.length - 1; i >= 0; i--) {
-        const char = s[i];
-        if (char === ")" || char === "*") {
-            stack.push(")");
-        } else if (char === "(") {
-            if (stack.length < 1) {
-                return false;
-            } else {
-                stack.pop();
-            }
+    while (openStack.length) {
+        if (!asteriskStack.length || asteriskStack.pop() < openStack.pop()) {
+            return false;
         }
-    };
+    }
 
     return true;
+
+    // Twisted Logic
+    // let stack = [];
+    // for (let i = 0; i < s.length; i++) {
+    //     const char = s[i];
+    //     if (char === "(" || char === "*") {
+    //         stack.push("(");
+    //     } else if (char === ")") {
+    //         if (stack.length < 1) {
+    //             return false;
+    //         } else {
+    //             stack.pop();
+    //         }
+    //     }
+    // };
+
+    // stack = [];
+    // for (let i = s.length - 1; i >= 0; i--) {
+    //     const char = s[i];
+    //     if (char === ")" || char === "*") {
+    //         stack.push(")");
+    //     } else if (char === "(") {
+    //         if (stack.length < 1) {
+    //             return false;
+    //         } else {
+    //             stack.pop();
+    //         }
+    //     }
+    // };
+
+    // return true;
 
     // Gready solution
     // let left_min = 0;
