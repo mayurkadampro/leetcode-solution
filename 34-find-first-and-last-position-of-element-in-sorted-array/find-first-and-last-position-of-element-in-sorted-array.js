08ef596b31319eca1f10e319126ea52ec6bfa0df
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-08 19:06:25 +0530

    Time: 55 ms (46.95%) | Memory: 49.9 MB (25.25%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,30 +4,29 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-    const binarySearch = function (nums, target, isSearchingLeft) {
-        let ans = -1;
-        let left = 0;
-        let right = nums.length - 1;
+    return [binarySearch(nums, target, true), binarySearch(nums, target, false)]
+};
 
-        while (left <= right) {
-            let mid = Math.floor(left + (right - left) / 2);
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
+
+const binarySearch = (nums, target, isFirstSearch) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let ans = -1;
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            ans = mid;
+            if (isFirstSearch) {
                 right = mid - 1;
             } else {
-                ans = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+                left = mid + 1;
             }
         }
-        return ans;
     }
 
-    const leftBinarySearch = binarySearch(nums, target, true);
-    const rightBinarySearch = binarySearch(nums, target, false);
-    return [leftBinarySearch, rightBinarySearch];
-};
+    return ans;
+}
