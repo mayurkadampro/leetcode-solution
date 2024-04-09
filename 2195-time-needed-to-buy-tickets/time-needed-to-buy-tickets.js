491800d22a563da491de94088462657d22df00d8
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-09 11:49:47 +0530

    Time: 50 ms (76.50%) | Memory: 49.2 MB (39.89%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+    let count = 0;
+    let index = 0;
+    while (tickets[k] !== 0) {
+        if (tickets[index] !== 0) {
+            tickets[index] = tickets[index] - 1;
+            count += 1;
+        }
+
+        if (tickets[k] === 0) {
+            return count;
+        }
+
+        if (index === tickets.length - 1) {
+            index = 0;
+        } else {
+            index += 1;
+        }
+    }
+
+    return count;
+};
