165a8d91ea5e65e5496bd9bbc690c9c443c0a972
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-12 19:54:49 +0530

    Time: 51 ms (69.10%) | Memory: 48.4 MB (90.90%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,12 +4,26 @@
  * @return {number}
  */
 var removeElement = function (nums, val) {
-    let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[count] = nums[i];
-            count++;
+    // let count = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] !== val) {
+    //         nums[count] = nums[i];
+    //         count++;
+    //     }
+    // }
+    // return count;
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        if (nums[left] === val) {
+            nums[left] = nums[right];
+            right--;
+        }
+        else {
+            left++;
         }
     }
-    return count;
+
+    return left;
 };
