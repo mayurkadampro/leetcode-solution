b9b57fbf3cb8cbaf8be3ea6e4ffc80575222cd95
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-12 20:00:55 +0530

    Time: 76 ms (24.42%) | Memory: 51.8 MB (87.31%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,20 +3,20 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    // let count = 1; // Initialize the count of unique elements to 1
-    // for (let i = 1; i < nums.length; i++) {
-    //     if (nums[i] !== nums[count - 1]) {
-    //         nums[count] = nums[i];
-    //         count++;
-    //     }
-    // }
-    // return count;
-
-    for (let i = 0; i < nums.length;) {
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i, 1)
-        } else {
-            i++
+    let count = 1; // Initialize the count of unique elements to 1
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[count - 1]) {
+            nums[count] = nums[i];
+            count++;
         }
     }
+    return count;
+
+    // for (let i = 0; i < nums.length;) {
+    //     if (nums[i] === nums[i + 1]) {
+    //         nums.splice(i, 1)
+    //     } else {
+    //         i++
+    //     }
+    // }
 };
