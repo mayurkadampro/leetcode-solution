83c27e816d8f837bc41504d07c5a66a0813c9dfb
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-14 11:07:40 +0530

    Time: 57 ms (74.45%) | Memory: 52.1 MB (50.31%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,13 +3,26 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-    let majorityCount = Math.floor(nums.length / 2);
-    let map = new Map();
-    for (let value of nums) {
-        map.set(value, (map.get(value) || 0) + 1);        
-        if(map.get(value) > majorityCount){
-            return value;
+    // let majorityCount = Math.floor(nums.length / 2);
+    // let map = new Map();
+    // for (let value of nums) {
+    //     map.set(value, (map.get(value) || 0) + 1);
+    //     if (map.get(value) > majorityCount) {
+    //         return value;
+    //     }
+    // }
+    // return 0;
+
+    // Boyer-Moore Voting Algorithm
+
+    let count = 0, candidate = 0
+
+    for (let num of nums) {
+        if (count == 0) {
+            candidate = num
         }
+        count += num == candidate ? 1 : -1
     }
-    return 0;
+
+    return candidate
 };
