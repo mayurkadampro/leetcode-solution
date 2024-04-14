c33048dcc9345bbf0474ffac74c436f472bbe88f
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-14 11:13:20 +0530

    Time: 72 ms (88.70%) | Memory: 55.8 MB (98.81%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,16 +4,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-    if (nums === null || nums.length === 0) {
-        return;
-    }
-    k = k % nums.length;
-    if (k === 0) {
-        return;
-    }
-    nums.reverse();
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, nums.length - 1);
+    // 1st solution
+    // if (nums === null || nums.length === 0) {
+    //     return;
+    // }
+    // k = k % nums.length;
+    // if (k === 0) {
+    //     return;
+    // }
+    // nums.reverse();
+    // reverse(nums, 0, k - 1);
+    // reverse(nums, k, nums.length - 1);
+
+    // 2nd soluution
+    const len = nums.length
+    k = (k % len)
+
+    let end = nums.splice(len - k)
+
+    nums.splice(0, 0, ...end)
 };
 
 const reverse = (arr, start, end) => {
