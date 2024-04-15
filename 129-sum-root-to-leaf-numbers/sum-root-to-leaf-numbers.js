bf5e2052a09a7ba4bf376d7f6e903083490f106e
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-15 11:03:13 +0530

    Time: 60 ms (29.36%) | Memory: 49.5 MB (72.21%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root, num = 0) {
+    if (!root) return 0;
+    num = num * 10 + root.val;
+    if (!root.left && !root.right) return num;
+    return sumNumbers(root.left, num) + sumNumbers(root.right, num);
+};
