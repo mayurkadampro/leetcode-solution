27cfb1d98c68394d8ced5d043ee73d24daca7a7d
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-15 11:27:05 +0530

    Time: 55 ms (99.69%) | Memory: 53.8 MB (24.74%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -2,51 +2,84 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function(matrix) {
+var setZeroes = function (matrix) {
     const n = matrix.length;
     const m = matrix[0].length;
 
-    let col0 = 1;
-    // Step 1: Traverse the matrix and mark 1st row & col accordingly:
+    // let col0 = 1;
+    // // Step 1: Traverse the matrix and mark 1st row & col accordingly:
+    // for (let i = 0; i < n; i++) {
+    //     for (let j = 0; j < m; j++) {
+    //         if (matrix[i][j] === 0) {
+    //             // Mark i-th row:
+    //             matrix[i][0] = 0;
+
+    //             // Mark j-th column:
+    //             if (j !== 0) {
+    //                 matrix[0][j] = 0;
+    //             } else {
+    //                 col0 = 0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    // for (let i = 1; i < n; i++) {
+    //     for (let j = 1; j < m; j++) {
+    //         if (matrix[i][j] !== 0) {
+    //             // Check for col & row:
+    //             if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+    //                 matrix[i][j] = 0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // // Step 3: Finally mark the 1st col & then 1st row:
+    // if (matrix[0][0] === 0) {
+    //     for (let j = 0; j < m; j++) {
+    //         matrix[0][j] = 0;
+    //     }
+    // }
+    // if (col0 === 0) {
+    //     for (let i = 0; i < n; i++) {
+    //         matrix[i][0] = 0;
+    //     }
+    // }
+
+    // return matrix;
+
+
+    let xArray = [];
+    let yArray = [];
+
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
             if (matrix[i][j] === 0) {
-                // Mark i-th row:
-                matrix[i][0] = 0;
-
-                // Mark j-th column:
-                if (j !== 0) {
-                    matrix[0][j] = 0;
-                } else {
-                    col0 = 0;
-                }
+                xArray.push(i);
+                yArray.push(j);
             }
         }
     }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (let i = 1; i < n; i++) {
-        for (let j = 1; j < m; j++) {
-            if (matrix[i][j] !== 0) {
-                // Check for col & row:
-                if (matrix[i][0] === 0 || matrix[0][j] === 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    // Step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] === 0) {
+    // Set All xArray rows Zero
+    for (let i = 0; i < xArray.length; i++) {
+        const index = xArray[i];
         for (let j = 0; j < m; j++) {
-            matrix[0][j] = 0;
+            matrix[index][j] = 0;
         }
-    }
-    if (col0 === 0) {
-        for (let i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
+
     }
 
-    return matrix;
+
+    // Set All yArray cols Zero
+    for (let i = 0; i < yArray.length; i++) {
+        const index = yArray[i];
+
+        for (let j = 0; j < n; j++) {
+            matrix[j][index] = 0;
+        }
+
+    }
 };
