814fc3766260ded1a452089b7784ad5d3e7c11be
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-17 14:05:22 +0530

    Time: 1866 ms (5.01%) | Memory: 57.8 MB (7.41%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -2,12 +2,41 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var canJump = function (nums) {
+//     let minIndexToWin = nums.length - 1;
+//     for (let i = nums.length - 1; i >= 0; i--) {
+//         if (i + nums[i] >= minIndexToWin) {
+//             minIndexToWin = i
+//         }
+//     }
+//     return minIndexToWin === 0;
+// };
+
+// DP Top-down Memoization
+// Time complexity : O(n^2)
+// Space complexity : O(2n) = O(n).
 var canJump = function (nums) {
-    let minIndexToWin = nums.length - 1;
-    for (let i = nums.length - 1; i >= 0; i--) {
-        if (i + nums[i] >= minIndexToWin) {
-            minIndexToWin = i
+    return helper(nums)
+}
+
+var helper = function (nums, position = 0, memo = []) {
+    // Base Case
+    if (position === nums.length - 1) {
+        return true;
+    }
+
+    // if its not valid return its false
+    if (memo[position] !== undefined) {
+        return memo[position];
+    }
+
+    let furthestJump = Math.min(position + nums[position], nums.length - 1);
+    for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+        if (helper(nums, nextPosition, memo)) {
+            memo[position] = true;
+            return true;
         }
     }
-    return minIndexToWin === 0;
-};
+    memo[position] = false;
+    return false;
+}
