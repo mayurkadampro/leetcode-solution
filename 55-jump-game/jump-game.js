df260826f7533ff9d05b6e4cadcd09ecc4708b0e
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-17 14:24:50 +0530

    Time: 63 ms (65.72%) | Memory: 53.4 MB (31.36%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -2,41 +2,41 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-// var canJump = function (nums) {
-//     let minIndexToWin = nums.length - 1;
-//     for (let i = nums.length - 1; i >= 0; i--) {
-//         if (i + nums[i] >= minIndexToWin) {
-//             minIndexToWin = i
-//         }
-//     }
-//     return minIndexToWin === 0;
-// };
+var canJump = function (nums) {
+    let minIndexToWin = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (i + nums[i] >= minIndexToWin) {
+            minIndexToWin = i
+        }
+    }
+    return minIndexToWin === 0;
+};
 
 // DP Top-down Memoization
 // Time complexity : O(n^2)
 // Space complexity : O(2n) = O(n).
-var canJump = function (nums) {
-    return helper(nums)
-}
+// var canJump = function (nums) {
+//     return helper(nums)
+// }
 
-var helper = function (nums, position = 0, memo = []) {
-    // Base Case
-    if (position === nums.length - 1) {
-        return true;
-    }
+// var helper = function (nums, position = 0, memo = []) {
+//     // Base Case
+//     if (position === nums.length - 1) {
+//         return true;
+//     }
 
-    // if its not valid return its false
-    if (memo[position] !== undefined) {
-        return memo[position];
-    }
+//     // if its not valid return its false
+//     if (memo[position] !== undefined) {
+//         return memo[position];
+//     }
 
-    let furthestJump = Math.min(position + nums[position], nums.length - 1);
-    for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-        if (helper(nums, nextPosition, memo)) {
-            memo[position] = true;
-            return true;
-        }
-    }
-    memo[position] = false;
-    return false;
-}
+//     let furthestJump = Math.min(position + nums[position], nums.length - 1);
+//     for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+//         if (helper(nums, nextPosition, memo)) {
+//             memo[position] = true;
+//             return true;
+//         }
+//     }
+//     memo[position] = false;
+//     return false;
+// }
