8724079ce561c329d098fbc58abf473368403497
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-18 11:56:51 +0530

    Time: 66 ms (78.76%) | Memory: 51.6 MB (81.72%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+
+};
+
+
+var maxAreaOfIsland = (grid) => {
+    let maxArea = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+        }
+    }
+    return maxArea;
+};
+
+const outOfBounds = (grid, r, c) => {
+    if (r < 0 || c < 0) return true;
+    if (r >= grid.length || c >= grid[0].length) return true;
+    if (grid[r][c] != 1) return true;
+    return false;
+}
+
+const dfs = (grid, row, col) => {
+    if (outOfBounds(grid, row, col)) return 0;
+
+    grid[row][col] = "";
+
+    var up = dfs(grid, row + 1, col);
+    var down = dfs(grid, row - 1, col);
+    var right = dfs(grid, row, col + 1);
+    var left = dfs(grid, row, col - 1);
+
+    return 1 + up + down + left + right;
+}
