1c2d8dfcb1a48eae2d2861e6df9f148dc4c2223a
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-19 10:33:40 +0530

    Time: 72 ms (60.87%) | Memory: 52.9 MB (55.18%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+
+const numIslands = function (grid) {
+    let count = 0;;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === "1") {
+                count++;
+                dfs(grid, i, j)
+            }
+        }
+    }
+    return count;
+};
+
+const outOfBounds = function (grid, r, c) {
+    if (r < 0 || c < 0) return true;
+    if (r >= grid.length || c >= grid[0].length) return true;
+    if (grid[r][c] != 1) return true;
+    return false;
+}
+
+const dfs = function (grid, row, col) {
+
+    if (outOfBounds(grid, row, col)) return 0;
+
+    grid[row][col] = "";
+
+    var up = dfs(grid, row + 1, col);
+    var down = dfs(grid, row - 1, col);
+    var right = dfs(grid, row, col + 1);
+    var left = dfs(grid, row, col - 1);
+
+    return 1 + up + down + left + right;
+}
