db5a618f6de1d9ba334c50af276c9dc679d812d6
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-20 16:33:27 +0530

    Time: 51 ms (64.99%) | Memory: 49.3 MB (21.20%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -8,6 +8,22 @@ var strStr = function (haystack, needle) {
     // return haystack.indexOf(needle);
 
     // Solution #2 - Find Position with Regex
-    const regex = new RegExp(needle);
-    return haystack.search(regex);
+    // const regex = new RegExp(needle);
+    // return haystack.search(regex);
+
+    // Solution #3 - Brute Force Time: O(N*M)
+    if (!needle) return 0;
+    if (haystack.length < needle.length) return -1;
+
+    for (let i = 0; i < haystack.length; i++) {
+        let isMatch = true;
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+        if (isMatch) return i;
+    }
+    return -1;
 };
