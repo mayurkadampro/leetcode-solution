ea165d525a2dfa3e3a4a517c369fe0e6fd666939
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-22 11:29:38 +0530

    Time: 66 ms (43.34%) | Memory: 52.1 MB (66.47%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    // Monotonic Stack solution
+    let stack = [];
+    for (const token of tokens) {
+        if (token.length === 1 && token.charCodeAt(0) < 48) {
+            const integer2 = stack.pop();
+            const integer1 = stack.pop();
+            stack.push(resolves(integer1, integer2, token));
+        } else {
+            stack.push(parseInt(token, 10));
+        }
+    }
+    return stack.pop();
+};
+
+var resolves = function (a, b, Operator) {
+    if (Operator === '+') return a + b;
+    else if (Operator === '-') return a - b;
+    else if (Operator === '*') return a * b;
+    return Math.trunc(a / b);
+}
