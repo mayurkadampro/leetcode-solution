27674b4303d5eed043bf179474eb253316848aa6
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-26 10:44:21 +0530

    Time: 72 ms (33.13%) | Memory: 53.9 MB (53.99%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.js
+++ 800-letter-case-permutation/letter-case-permutation.js
@@ -11,27 +11,19 @@ const letterCasePermutation = function (s) {
 const helper = function (s, result, str, index) {
 
     // base case
-    if (index === s.length) {
+    if (index === s.length || str.length === s.length) {
         result.push(str);
         return;
     } else if (isAlpha(s[index])) {
 
-        
-        
-        // in the case the current character is an upper alphabet, we want a permutation with a lower alphabet
-        if (s.charAt(index) >= 'A' && s.charAt(index) <= 'Z') {
-            helper(s, result, str + s.charAt(index).toLowerCase(), index + 1)
-        }
-        // in the case the current character is a lower alphabet we want a permutation with an upper alphabet
-        if (s.charAt(index) >= 'a' && s.charAt(index) <= 'z') {
-            helper(s, result, str + s.charAt(index).toUpperCase(), index + 1)
-        }
-
+        let up = str + s[index].toUpperCase();
+        let low = str + s[index].toLowerCase();
+        helper(s, result, up, index + 1);
+        helper(s, result, low, index + 1);
+    } else {
+        // no matter what we proceed with the original permutation
+        helper(s, result, str + s[index], index + 1);
     }
-
-
-    // no matter what we proceed with the original permutation
-    helper(s, result, str + s.charAt(index), index + 1);
 }
 
 const isAlpha = function (ch) {
