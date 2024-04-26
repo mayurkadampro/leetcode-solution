d3b3469c44c38c6ebfafc1c7ada33f23530cc289
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-26 10:39:03 +0530

    Time: 66 ms (65.64%) | Memory: 52.3 MB (91.41%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+const letterCasePermutation = function (s) {
+    const result = []
+    helper(s, result, "", 0);
+    return result;
+};
+
+const helper = function (s, result, str, index) {
+
+    // base case
+    if (index === s.length) {
+        result.push(str);
+        return;
+    } else if(isAlpha(s.charAt(index))) {
+        // in the case the current character is an upper alphabet, we want a permutation with a lower alphabet
+        if (s.charAt(index) >= 'A' && s.charAt(index) <= 'Z') {
+            helper(s, result, str + s.charAt(index).toLowerCase(), index + 1)
+        }
+        // in the case the current character is a lower alphabet we want a permutation with an upper alphabet
+        if (s.charAt(index) >= 'a' && s.charAt(index) <= 'z') {
+            helper(s, result, str + s.charAt(index).toUpperCase(), index + 1)
+        }
+    }
+
+
+    
+    helper(s, result, str + s.charAt(index), index + 1);
+}
+
+const isAlpha = function (ch) {
+    return /[a-zA-Z]/i.test(ch)
+}
