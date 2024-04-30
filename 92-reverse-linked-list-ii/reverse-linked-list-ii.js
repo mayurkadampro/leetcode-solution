4924a66a8602ab5b65f4974c20ede6c8e0c96c41
Mayur Kadam <mayurkadam200@gmail.com>
2024-04-30 11:22:45 +0530

    Time: 58 ms (22.21%) | Memory: 48.4 MB (93.03%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function (list, left, right) {
+    const dummy = new ListNode(0, list);
+    let head = dummy;
+    let curr = 1;
+
+    while (curr++ < left) { 
+        
+        head = head.next 
+    };
+
+    let tail = head.next;
+
+
+    while (left < right) {
+        left++;
+        const next = tail.next;
+        tail.next = next.next;
+        next.next = head.next;
+        head.next = next;
+    }
+
+    return dummy.next;
+};
