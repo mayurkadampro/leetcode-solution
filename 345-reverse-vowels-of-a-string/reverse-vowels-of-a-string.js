0a962ce377fa1f0a76344facdc64671662f46ef9
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-01 20:33:06 +0530

    Time: 62 ms (80.31%) | Memory: 54 MB (90.87%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+    let set = new Set();
+    set.add('a');
+    set.add('e');
+    set.add('i');
+    set.add('o');
+    set.add('u');
+    set.add('A');
+    set.add('E');
+    set.add('I');
+    set.add('O');
+    set.add('U');
+
+    const arr = s.split("");
+    while (left <= right) {
+        let leftStr = arr[left];
+        let rightStr = arr[right];
+        if (!set.has(leftStr)) {
+            left++;
+        } else if (!set.has(rightStr)) {
+            right--;
+        } else if (set.has(leftStr) && set.has(rightStr)) {
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    return arr.join("");
+};
