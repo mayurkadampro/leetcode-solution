43517392037015130c19b7051c1a3302f36a770e
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-01 20:03:01 +0530

    Time: 45 ms (89.69%) | Memory: 49.2 MB (26.46%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} word
+ * @param {character} ch
+ * @return {string}
+ */
+var reversePrefix = function (word, ch) {
+    let chIndex = word.indexOf(ch);
+    if (chIndex < 0) return word
+    let firstWord = word.substring(0, chIndex + 1);
+    let secondWord = word.substring(chIndex + 1, word.length);
+    
+    return reverseStr(firstWord) + secondWord;
+};
+
+const reverseStr = (str) => {
+    let strRev = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        strRev += str[i];
+    }
+    return strRev;
+}
