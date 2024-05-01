249806e43eb1fcd4efef7bdf29671a4ff12ba407
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-01 20:14:02 +0530

    Time: 44 ms (92.41%) | Memory: 48.8 MB (57.98%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
@@ -4,11 +4,27 @@
  * @return {string}
  */
 var reversePrefix = function (word, ch) {
-    let chIndex = word.indexOf(ch);
-    if (chIndex < 0) return word
-    let firstWord = word.slice(0, chIndex + 1);
-    let secondWord = word.slice(chIndex + 1, word.length);
-    return reverseStr(firstWord) + secondWord;
+    // 1st solution
+    // let chIndex = word.indexOf(ch);
+    // if (chIndex < 0) return word
+    // let firstWord = word.slice(0, chIndex + 1);
+    // let secondWord = word.slice(chIndex + 1, word.length);
+    // return reverseStr(firstWord) + secondWord;
+
+    // 2nd solution by swapping
+    let left = 0;
+    let right = word.indexOf(ch);
+    if (right < 0 || word === ch) return word;
+    const arr = word.split('');
+    while (left < right) {
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+
+    return arr.join('');
 };
 
 const reverseStr = (str) => {
