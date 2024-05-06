36b601d4267f0afa9bdee88a6ad4ff0ec91d16a3
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-06 10:50:58 +0530

    Time: 277 ms (56.36%) | Memory: 95.1 MB (23.64%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.js
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.js
@@ -10,12 +10,8 @@
  * @return {ListNode}
  */
 var removeNodes = function (head) {
-
     if (!head) return null;
-
     head.next = removeNodes(head.next);
-
-    if (head.next && head.val < head.next.val) return head.next
-
+    if (head.next && head.val < head.next.val) return head.next;
     return head;
 };
