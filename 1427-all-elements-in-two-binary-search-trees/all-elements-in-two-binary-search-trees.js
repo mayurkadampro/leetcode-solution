ff60be08893cfa6583a222ee85979bb61a48275d
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-12 10:33:24 +0530

    Time: 110 ms (100.00%) | Memory: 61.6 MB (52.17%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.js
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.js
@@ -13,11 +13,9 @@
  */
 var getAllElements = function (root1, root2) {
     let arr1 = [];
-    let arr2 = [];
     traversal(root1, arr1);
-    traversal(root2, arr2);
-    arr1 = [...arr1, ...arr2].sort((a, b) => a - b);
-    return arr1;
+    traversal(root2, arr1);
+    return arr1.sort((a, b) => a - b);
 };
 
 // inorder traversal
