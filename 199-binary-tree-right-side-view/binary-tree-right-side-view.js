0249a2b724441256d52ff57d1366add2c256fb9d
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-14 15:56:43 +0530

    Time: 67 ms (17.57%) | Memory: 50.6 MB (93.21%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -11,15 +11,31 @@
  * @return {number[]}
  */
 var rightSideView = function (root) {
-    if (!root) return [];
-    let res = [];
-    pre(root, 0);
-    return res;
+    // DFS Solution
+    // if (!root) return [];
+    // let res = [];
+    // pre(root, 0);
+    // return res;
 
-    function pre(node, h) {
-        if (!node) return;
-        res[h] = node.val;
-        pre(node.left, h + 1);
-        pre(node.right, h + 1);
+    // function pre(node, h) {
+    //     if (!node) return;
+    //     res[h] = node.val;
+    //     pre(node.left, h + 1);
+    //     pre(node.right, h + 1);
+    // }
+
+    // BFS SOLUTION
+    if (!root) return [];
+    const q = [root];
+    const result = [];
+    while (q.length) {
+        const size = q.length;
+        result.push(q[size - 1].val);
+        for (let i = 0; i < size; i++) {
+            const node = q.shift();
+            node.left && q.push(node.left);
+            node.right && q.push(node.right);
+        }
     }
+    return result;
 };
