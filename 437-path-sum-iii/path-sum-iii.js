839be047355253e5ae0481ba5247fa7b6accd48b
Mayur Kadam <mayurkadam200@gmail.com>
2024-05-19 12:55:22 +0530

    Time: 62 ms (91.53%) | Memory: 53.3 MB (85.17%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.js
+++ 437-path-sum-iii/path-sum-iii.js
@@ -12,29 +12,55 @@
  * @return {number}
  */
 
+// Brute Force
+// var pathSum = function (root, targetSum) {
+//     let total = 0;
+
+//     const helper = (root, targetSum) => {
+//         if (!root) return root;
+//         findPathSum(root, targetSum);
+//         helper(root.left, targetSum);
+//         helper(root.right, targetSum);
+//     }
+
+//     const findPathSum = (root, targetSum) => {
+//         if (!root) return root;
+
+//         if (root.val === targetSum) {
+//             total++;
+//         }
+
+//         return findPathSum(root.left, targetSum - root.val) || findPathSum(root.right, targetSum - root.val);
+//     }
+
+//     helper(root, targetSum);
+//     return total;
+// };
+
+// With HashpMap Gain
 var pathSum = function (root, targetSum) {
     let total = 0;
+    let sumMap = new Map();
+    sumMap.set(0, 1);
 
-    const helper = (root, targetSum) => {
-        if (!root) return root;
-        findPathSum(root, targetSum);
-        helper(root.left, targetSum);
-        helper(root.right, targetSum);
-    }
-
-    const findPathSum = (root, targetSum) => {
+    const helper = (root, currSum) => {
         if (!root) return root;
 
-        if (root.val === targetSum) {
-            total++;
+        currSum += root.val;
+        if (sumMap.has(currSum - targetSum)) {
+            total += sumMap.get(currSum - targetSum);
         }
+        sumMap.set(currSum, (sumMap.get(currSum) || 0) + 1);
+        helper(root.left, currSum);
+        helper(root.right, currSum);
+        sumMap.set(currSum, sumMap.get(currSum) - 1)
+    };
 
-        return findPathSum(root.left, targetSum - root.val) || findPathSum(root.right, targetSum - root.val);
-    }
-
-    helper(root, targetSum);
+    helper(root, 0);
     return total;
 };
+
+
 
 
 
