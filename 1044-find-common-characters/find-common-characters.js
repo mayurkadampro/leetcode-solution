9601eadf6c2d5485ce4417c7412da814d302cff7
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-05 12:26:17 +0530

    Time: 59 ms (75.00%) | Memory: 51.8 MB (72.80%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function (words) {
+    let result = [];
+    helper(words, result);
+    return result;
+};
+
+const helper = (words, result) => {
+    if (words.length === 1) return;
+
+    let minFreq = new Array(26).fill(Infinity);
+
+    for (const word of words) {
+        let freq = new Array(26).fill(0);
+        for (const c of word) {
+            freq[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+        }
+
+        for (let i = 0; i < 26; i++) {
+            minFreq[i] = Math.min(minFreq[i], freq[i]);
+        }
+    }
+
+    for (let i = 0; i < 26; i++) {
+        while (minFreq[i] > 0) {
+            result.push(String.fromCharCode(i + 'a'.charCodeAt(0)));
+            minFreq[i]--;
+        }
+    }
+
+    return result;
+}
