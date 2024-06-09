a39e087b926077b63484fcece4810c15eeb64421
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-09 12:44:44 +0530

    Time: 86 ms (23.47%) | Memory: 57.6 MB (18.37%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
@@ -4,21 +4,32 @@
  * @return {number}
  */
 var subarraysDivByK = function (nums, k) {
-    let count = 0;
+    // Initialize the sum, count as zero
     let sum = 0;
-    let kMap = new Map();
-    kMap.set(0, 1);
+    let count = 0;
+
+    // Initialize map to store the first occurrence of remainders
+    const kMap = new Map();
+    kMap.set(0, 1); // Initialize the map with remainder 0 at index -1.
+
 
     for (let value of nums) {
-        sum += value;
-        let reminder = sum % k;
-        if (reminder < 0) {
-            reminder += k;
+        sum += value; // Increment the sum by the current element.
+
+        // Calculate the remainder of the sum with respect to k.
+        const remainder = sum % k;
+
+        // Adjust remainder to be non-negative.
+        const adjustedRemainder = remainder < 0 ? remainder + k : remainder;
+
+        // If the remainder has been seen before.
+        if (kMap.has(adjustedRemainder)) {
+            count += kMap.get(adjustedRemainder);
         }
-        if (kMap.has(reminder)) {
-            count += kMap.get(reminder);
-        }
-        kMap.set(reminder, (kMap.get(reminder) || 0) + 1);
+
+        // Store the first occurrence of the remainder.
+        kMap.set(adjustedRemainder, (kMap.get(adjustedRemainder) || 0) + 1);
+
     }
     return count;
 };
