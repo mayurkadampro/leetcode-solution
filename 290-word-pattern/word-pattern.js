61fbdb3a014c8c009610a59d6794f96f7053c012
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-15 16:54:47 +0530

    Time: 48 ms (75.99%) | Memory: 48.7 MB (53.32%) - LeetSync

--- 290-word-pattern/word-pattern.js
+++ 290-word-pattern/word-pattern.js
@@ -10,18 +10,17 @@ var wordPattern = function (pattern, s) {
     if (pattern.length != str.length) return false;
     if (new Set(str).size != new Set(pattern).size) return false;
 
-    // for (let i = 0; i < str.length; i++) {
-    //     if (!map.has(pattern[i])) {
-    //         map.set(pattern[i], str[i]);
-    //     }
-    // }
+    for (let i = 0; i < str.length; i++) {
+        if (!map.has(pattern[i])) {
+            map.set(pattern[i], str[i]);
+        }
+    }
 
 
     for (let i = 0; i < pattern.length; i++) {
-        if (map.has(pattern[i]) && map.get(pattern[i]) !== str[i]) {
+        if (!map.has(pattern[i]) || map.get(pattern[i]) !== str[i]) {
             return false;
         }
-        map.set(pattern[i], str[i]);
     }
 
     return true;
