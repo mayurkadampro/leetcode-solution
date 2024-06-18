43ed4edc605545331338e04939c51bc7fc9d290e
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-18 10:29:51 +0530

    Time: 66 ms (62.65%) | Memory: 53.4 MB (50.51%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.js
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.js
@@ -14,18 +14,17 @@ var detectCycle = function (head) {
     let slow = head;
     let fast = head;
 
-    while (fast && fast.next) {
+    while (fast !== null && fast.next !== null) {
         slow = slow.next;
         fast = fast.next.next;
         if (slow === fast) break;
     }
 
-    if (fast === null || fast.next === null) return null;
+    if (fast == null || fast.next == null) return null;
 
-    while (head !== slow) {
-        head = head.next;
-        slow = slow.next;
+    while (head != slow) {
+        head = head.next;   // moving head by 1...
+        slow = slow.next;   // moving slow by 1 as well...
     }
-
-    return head;
+    return head; // Or return slow, they both will return the tail where cycle starts...
 };
