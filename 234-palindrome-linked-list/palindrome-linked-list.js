c107d3b5468f154b49371905aad0240dff53bb38
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-19 11:13:42 +0530

    Time: 117 ms (82.76%) | Memory: 63.8 MB (98.45%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -10,32 +10,62 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-    // Find Mid Point;
+    // 1st solution
+    // // Find Mid Point;
+    // let slow = head;
+    // let fast = head;
+    // while (fast && fast.next) {
+    //     slow = slow.next;
+    //     fast = fast.next.next;
+    // }
+
+    // //  Reverse list from mid point
+    // let prev = null;
+    // let next = slow;
+    // while (slow != null) {
+    //     next = slow.next;
+    //     slow.next = prev;
+    //     prev = slow;
+    //     slow = next
+    // }
+
+    // let left = head;
+    // let right = prev;
+    // while (right != null) {
+    //     if (left.val !== right.val) {
+    //         return false;
+    //     }
+    //     left = left.next;
+    //     right = right.next;
+    // }
+    // return true;
+
+    // 2nd solution
     let slow = head;
     let fast = head;
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-
-    //  Reverse list from mid point
     let prev = null;
-    let next = slow;
-    while (slow != null) {
-        next = slow.next;
+
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        // reverse
+        let temp = slow.next;
         slow.next = prev;
-        prev = slow;
-        slow = next
+        prev = slow
+        slow = temp;
     }
 
-    let left = head;
-    let right = prev;
-    while (right != null) {
-        if (left.val !== right.val) {
-            return false;
-        }
-        left = left.next;
-        right = right.next;
+    // check linkedlist is even or odd and move slow pointer
+    if (fast !== null) {
+        slow = slow.next;
     }
+
+    while (slow !== null) {
+        if (prev.val !== slow.val) {
+            return false
+        }
+        slow = slow.next;
+        prev = prev.next;
+    }
+
     return true;
 };
