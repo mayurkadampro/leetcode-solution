b2851351d30702c445dc8c7924056cbe0cd53ef2
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-22 12:10:28 +0530

    Time: 58 ms (47.61%) | Memory: 50.1 MB (83.63%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -11,11 +11,39 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function (head, n) {
-    let fast = head, slow = head
-    for (let i = 0; i < n; i++) fast = fast.next;
-    if (!fast) return head.next
+    // let fast = head, slow = head
+    // for (let i = 0; i < n; i++) fast = fast.next;
+    // if (!fast) return head.next
 
-    while (fast.next) fast = fast.next, slow = slow.next
-    slow.next = slow.next.next
-    return head
+    // while (fast.next) fast = fast.next, slow = slow.next
+    // slow.next = slow.next.next
+    // return head;
+
+    let len = 0;
+    let current = head;
+
+    while (current) {
+        len++;
+        current = current.next;
+    }
+
+    let matchIdx = len - (n - 1);
+    let dummyNode = new ListNode(-1, null);
+    dummyNode.next = head;
+    let prev = dummyNode
+    current = head;
+    len = 0;
+    while (current) {
+        len++;
+        if (len === matchIdx) {
+            prev.next = current.next;
+
+        } else {
+            prev = current;
+        }
+
+        current = current.next;
+    }
+
+    return dummyNode.next;
 };
