a454a60fc5d72499e7d2389fb654509be780e80b
Mayur Kadam <mayurkadam200@gmail.com>
2024-06-22 11:36:59 +0530

    Time: 78 ms (67.32%) | Memory: 59.9 MB (11.88%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -12,10 +12,57 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function (headA, headB) {
-    let a = headA, b = headB;
-    while (a !== b) {
-        a = !a ? headB : a.next
-        b = !b ? headA : b.next
+    // let a = headA, b = headB;
+    // while (a !== b) {
+    //     a = !a ? headB : a.next
+    //     b = !b ? headA : b.next
+    // }
+    // return a;
+
+    let current = headA;
+    let lenOfA = 0;
+    while (current) {
+        lenOfA++;
+        current = current.next;
     }
-    return a;
-};
+
+    current = headB
+    let lenOfB = 0;
+    while (current) {
+        lenOfB++;
+        current = current.next;
+    }
+
+    let diff = Math.abs(lenOfA - lenOfB);
+    if (lenOfA > lenOfB) {
+        current = headA;
+        while (diff > 0) {
+            diff--;
+            current = current.next;
+        }
+
+        while (current) {
+            if(current === headB) return current;
+            current = current.next;
+            headB = headB.next;
+        }
+
+    } else {
+        current = headB;
+        while (diff > 0) {
+            diff--;
+            current = current.next;
+        }
+
+        while (current) {
+            if(current === headA) return current;
+            current = current.next;
+            headA = headA.next;
+        }
+    }
+
+
+
+
+
+};  
