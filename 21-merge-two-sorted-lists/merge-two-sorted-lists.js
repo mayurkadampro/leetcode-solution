6b4657e2a69dfc77ffbcb345f24f1ec8dee0cf5a
Mayur Kadam <mayurkadam200@gmail.com>
2024-07-06 12:03:45 +0530

    Time: 57 ms (81.69%) | Memory: 51.7 MB (39.70%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -23,19 +23,34 @@ var mergeTwoLists = function (list1, list2) {
     // }
 
     // # iteratively
-    let dummyNode = { val: -1, next: null };
+    if (!list1) return list2;
+    if (!list2) return list1;
+    let dummyNode = new ListNode(-1, null);
     let current = dummyNode;
 
     while (list1 && list2) {
-        if (list1.val < list2.val) {
+        let list1Val = list1 ? list1.val : null;
+        let list2Val = list2 ? list2.val : null;
+        
+        if (list1Val < list2Val) {
             current.next = list1;
             list1 = list1.next;
         } else {
             current.next = list2;
             list2 = list2.next;
         }
+
         current = current.next;
     }
-    current.next = list1 || list2;
+
+    if (list1) {
+        current.next = list1;
+    }
+
+    if (list2) {
+        current.next = list2;
+    }
+
     return dummyNode.next;
+
 };
