5d05880cd0d596de3972d9d8a5e772912f1a7e44
Mayur Kadam <mayurkadam200@gmail.com>
2024-07-07 12:05:06 +0530

    Time: 88 ms (69.28%) | Memory: 55.7 MB (46.88%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -10,30 +10,76 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var mergeKLists = function (lists) {
-    if (!lists) return [];
+    if (lists === null || lists.length === 0) return null;
 
-    const minPriorityQueue = new MinPriorityQueue({ priority: x => x.val });
-
-    for (const head of lists) {
-        if (head) {
-            minPriorityQueue.enqueue(head);
+    // TC : O(N * logK), SC : O(1)
+    let interval = 1;
+    while (interval < lists.length) {
+        for (let i = 0; i + interval <= lists.length; i = i + interval * 2) {
+            lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
         }
+        interval = interval * 2;
     }
+    
+    return lists[0] ? lists[0] : null;
 
+    // TC : O(N*K), SC : O(N)
+    // const minPriorityQueue = new MinPriorityQueue({ priority: x => x.val });
+
+    // for (const head of lists) {
+    //     if (head) {
+    //         minPriorityQueue.enqueue(head);
+    //     }
+    // }
+
+    // let dummyNode = new ListNode(-1, null);
+    // let current = dummyNode;
+
+    // while (!minPriorityQueue.isEmpty()) {
+    //     const { val, next } = minPriorityQueue.dequeue().element;
+
+    //     current.next = new ListNode(val, next);
+    //     current = current.next;
+
+    //     if (next) {
+    //         minPriorityQueue.enqueue(next);
+    //     }
+
+    // }
+
+    // return dummyNode.next;
+};
+
+var mergeTwoLists = function (list1, list2) {
+    // # iteratively
+    if (!list1) return list2;
+    if (!list2) return list1;
     let dummyNode = new ListNode(-1, null);
     let current = dummyNode;
 
-    while (!minPriorityQueue.isEmpty()) {
-        const { val, next } = minPriorityQueue.dequeue().element;
+    while (list1 && list2) {
+        let list1Val = list1 ? list1.val : null;
+        let list2Val = list2 ? list2.val : null;
 
-        current.next = new ListNode(val, next);
-        current = current.next;
-
-        if (next) {
-            minPriorityQueue.enqueue(next);
+        if (list1Val < list2Val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
         }
 
+        current = current.next;
+    }
+
+    if (list1) {
+        current.next = list1;
+    }
+
+    if (list2) {
+        current.next = list2;
     }
 
     return dummyNode.next;
+
 };
